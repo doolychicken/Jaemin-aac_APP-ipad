ed3df5bf4cac9fd515c6cccd13e4a45de2c25467
doolychicken <doolychicken@naver.com>
2026-07-03 14:31:59 +0900

    Double iPad puzzle number size

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * On first visit, pre-caches all images so subsequent loads are instant.
  */
 
-const CACHE_VERSION = 'v227';
+const CACHE_VERSION = 'v228';
 const CACHE_NAME = `jaemin-aac-${CACHE_VERSION}`;
 
 self.addEventListener('message', (event) => {
