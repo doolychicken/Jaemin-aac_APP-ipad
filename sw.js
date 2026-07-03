83f185d4d343bf94be3b86beaeab70115a315295
doolychicken <doolychicken@naver.com>
2026-07-03 16:12:16 +0900

    Rebalance people puzzle layout

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * On first visit, pre-caches all images so subsequent loads are instant.
  */
 
-const CACHE_VERSION = 'v239';
+const CACHE_VERSION = 'v240';
 const CACHE_NAME = `jaemin-aac-${CACHE_VERSION}`;
 
 self.addEventListener('message', (event) => {
