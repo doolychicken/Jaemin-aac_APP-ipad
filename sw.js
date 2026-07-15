9ce36aaebeae6d38950734198214111a9b5bbb71
doolychicken <doolychicken@naver.com>
2026-07-15 12:56:45 +0900

    Enlarge face puzzle choices

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * On first visit, pre-caches all images so subsequent loads are instant.
  */
 
-const CACHE_VERSION = 'v316';
+const CACHE_VERSION = 'v317';
 const CACHE_NAME = `jaemin-aac-${CACHE_VERSION}`;
 
 self.addEventListener('message', (event) => {
